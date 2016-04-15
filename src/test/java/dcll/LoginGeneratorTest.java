fbbff7e72b62e7db4ff523d30d2e3543dc5ed450
lovajenny <lovajenny.andriana@gmail.com>
2016-04-15 15:07:32 +0200

    Test unitaire de la classe LoginGenerator valide

---
+++ src/test/java/dcll/LoginGeneratorTest.java
@@ -0,0 +1,62 @@
+package dcll;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by lova on 15/04/16.
+ */
+public class LoginGeneratorTest {
+
+    @Test
+    public void testGenerateLoginForNomAndPrenom() throws Exception {
+
+        // Pour Paul Durand
+        //Given
+        LoginService loginService = new LoginService(new String[]
+                {"JROL", "BPER", "CGUR", "JDUP", "JRAL", "JRAL1"});
+        LoginGenerator loginGenerator = new LoginGenerator(loginService);
+        String loginPaul = "";
+
+        //When
+        loginPaul = loginGenerator.generateLoginForNomAndPrenom("Durand", "Paul");
+
+        //Then
+        assertEquals(loginPaul, "PDUR");
+    }
+
+    @Test
+    public void testGenerateLoginForNomAndPrenomDupli() throws Exception {
+
+        // Pour Jean Rolling
+        //Given
+        LoginService loginService = new LoginService(new String[]
+                {"JROL", "BPER", "CGUR", "JDUP", "JRAL", "JRAL1"});
+        LoginGenerator loginGenerator = new LoginGenerator(loginService);
+        String loginJean = "";
+
+        //When
+        loginJean = loginGenerator.generateLoginForNomAndPrenom("Rolling", "Jean");
+
+        //Then
+        assertEquals(loginJean, "JROL1");
+    }
+
+    @Test
+    public void testGenerateLoginForNomAndPrenomAccent() throws Exception {
+
+        // Pour Paul Dùrand
+        //Given
+        LoginService loginService = new LoginService(new String[]
+                {"JROL", "BPER", "CGUR", "JDUP", "JRAL", "JRAL1"});
+        LoginGenerator loginGenerator = new LoginGenerator(loginService);
+        String loginPaul2 = "";
+
+        //When
+        loginPaul2 = loginGenerator.generateLoginForNomAndPrenom("Dùrand", "Paul");
+
+        //Then
+        assertEquals(loginPaul2, "PDUR");
+    }
+}
